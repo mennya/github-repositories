878c0bdc8e04c95b1931f97faa99dc128c7c5cf1
menya <mennya13@gmail.com>
2019-02-25 10:55:36 +0500

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Component} from '@angular/core';
+import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'github-repositories';
+
+  public readonly options = {headers: new HttpHeaders({'Content-Type': 'application/json;charset=UTF-8'})};
+  public repos;
+  public searchModel;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  public search(e) {
+    if (e.key === 'Enter') {
+      this.http.get(`https://api.github.com/repositories?q=${this.searchModel}`, this.options)
+        .subscribe((data) => this.repos = data);
+    }
+  }
+
+  public selectRepo() {
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {MatButtonModule, MatCardModule, MatCommonModule, MatFormFieldModule, MatInputModule, MatListModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -10,9 +15,21 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    CommonModule,
+    MatCommonModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatButtonModule,
+    MatListModule,
+    MatCardModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
