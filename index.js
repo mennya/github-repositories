f786dc8896dac54e25e347dd7055456656fdd182
menya <79504406065@ya.ru>
2019-02-25 11:27:02 +0500

    fix deploy to heroku

--- index.js
+++ index.js
@@ -4,20 +4,16 @@ const http = require('http');
 const dist = __dirname + '/dist';
 const PORT = process.env.PORT || 3400;
 
-app.use(function (req, res, next) {
-
+app.use((req, res, next) => {
   if ('production' === process.env.NODE_ENV) {
-
     if (req.url.indexOf('.js') > -1) {
       res.setHeader('Content-Encoding', 'gzip');
       res.removeHeader('Content-Length');
       req.url += '.gz';
     }
-
   }
 
   next();
-
 });
 
 app.use('/', express.static(dist));
