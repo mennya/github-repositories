325695d5f0d94e8f8519b928ad50db93de8700f7
menya <79504406065@ya.ru>
2019-02-26 12:39:51 +0500

    use HttpParams

--- src/app/blocks/api/github/github.service.ts
+++ src/app/blocks/api/github/github.service.ts
@@ -1,4 +1,4 @@
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 
 @Injectable()
@@ -8,7 +8,7 @@ export class GithubService {
   }
 
   public getRepositories(search: string) {
-    return this.http.get(`${this.baseUrl}search/repositories?q=${search}`);
+    return this.http.get(`${this.baseUrl}search/repositories`, {params: new HttpParams().set('q', search)});
   }
 
   public getBranches(login: string, repo: string) {
